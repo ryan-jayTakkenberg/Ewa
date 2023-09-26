2d470116a80ecfaafd654c8ec5be3d84b7d443f9
Léon Dolmans <59891470+leond3@users.noreply.github.com>
2023-09-26 14:41:40 +0200

    Added google fonts (https://fonts.google.com/icons?selected=Material+Symbols+Outlined:home:FILL@0;wght@400;GRAD@0;opsz@24)

--- frontend/src/AppComponent.vue
+++ frontend/src/AppComponent.vue
@@ -13,4 +13,12 @@ export default {
 </script>
 
 <style>
+/* https://fonts.google.com/icons?selected=Material+Symbols+Outlined:home:FILL@0;wght@400;GRAD@0;opsz@24 */
+.material-symbols-outlined {
+  font-variation-settings:
+      'FILL' 0,
+      'wght' 400,
+      'GRAD' 0,
+      'opsz' 24
+}
 </style>
