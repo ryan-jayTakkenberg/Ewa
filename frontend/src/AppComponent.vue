e5493e9aeba810dcb9cf85a442883b06db1e45da
Tobi <tobi.kok@hva.nl>
2023-09-28 20:00:24 +0200

    Montserrat font added (Solar Guidelines)

--- frontend/src/AppComponent.vue
+++ frontend/src/AppComponent.vue
@@ -1,27 +1,29 @@
 <template>
   <router-view></router-view>
-  <nav-bar-component></nav-bar-component>
+<!--  <nav-bar-component></nav-bar-component>-->
 </template>
 
 <script>
 
-import NavBarComponent from "@/components/NavBarComponent.vue";
+// import NavBarComponent from "@/components/NavBarComponent.vue";
+
 
 export default {
   name: 'AppComponent',
   components: {
-    NavBarComponent
-
+    // NavBarComponent
   }
 }
 </script>
 
 <style>
-/** {*/
-/*  outline: 1px solid red;*/
-/*}*/
-* {
+
+*, *::before, *::after {
   margin: 0;
   padding: 0;
+  box-sizing: border-box;
+  font-family: "Montserrat", sans-serif;
+  //outline: 1px solid red;
 }
+
 </style>
