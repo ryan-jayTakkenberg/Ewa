7e599f2635080d5b394236a2f7e4be011a87e246
leond3 <leon.dolmans@hva.nl>
2023-10-02 21:20:55 +0200

    cleanup, ability to unselect, mobile support

--- frontend/src/components/ManageComponent.vue
+++ frontend/src/components/ManageComponent.vue
@@ -1,100 +1,58 @@
 <template>
-<!--  <nav class="navbar navbar-light bg-light">-->
-<!--    <div class="empty"></div>-->
-<!--    <div class="container rounded-3" @click="goToProductDetail">-->
-<!--      <div class="logoContainer">-->
-<!--        <span class="material-symbols-outlined">inventory_2</span>-->
-<!--      </div>-->
-<!--      <div class="textContainer rounded-bottom-3" style="background-color:aqua;">-->
-<!--        <span class="fw-semibold">Product</span>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--    <div class="container rounded-3" @click="goToWarehouseDetail">-->
-<!--      <div class="logoContainer">-->
-<!--        <span class="material-symbols-outlined">warehouse</span>-->
-<!--      </div>-->
-<!--      <div class="textContainer rounded-bottom-3" style="background-color:yellow;">-->
-<!--        <span class="fw-semibold">Warehouses</span>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--    <div class="container rounded-3" @click="goToProjectDetail">-->
-<!--      <div class="logoContainer">-->
-<!--        <span class="material-symbols-outlined">tactic</span>-->
-<!--      </div>-->
-<!--      <div class="textContainer rounded-bottom-3" style="background-color:orangered;">-->
-<!--        <span class="fw-semibold">Projects</span>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--    <div class="container rounded-3">-->
-<!--      <div class="logoContainer">-->
-<!--        <span class="material-symbols-outlined">folder_shared</span>-->
-<!--      </div>-->
-<!--      <div class="textContainer rounded-bottom-3" style="background-color:limegreen;">-->
-<!--        <span class="fw-semibold">Teams</span>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--    <div class="container rounded-3">-->
-<!--      <div class="logoContainer">-->
-<!--        <span class="material-symbols-outlined">person</span>-->
-<!--      </div>-->
-<!--      <div class="textContainer rounded-bottom-3" style="background-color:orange;">-->
-<!--        <span class="fw-semibold">Accounts</span>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--    <div class="empty"></div>-->
-<!--  </nav>-->
-<!--<div>-->
-<!--  <router-view>-->
-<!--  </router-view>-->
-<!--</div>-->
+  <NavBarComponent></NavBarComponent>
   <div>
     <div class="nav-box">
-      <div @click="goToProductDetail" class="content-box">
+      <div @click="goToPage('product')" class="content-box">
         <div class="content-box-top"><span class="material-symbols-outlined">inventory_2</span></div>
-        <div class="content-box-bottom product"><h3>Product</h3></div>
+        <div class="content-box-bottom product"><h3 class="subtitle">Product</h3></div>
       </div>
-      <div @click="goToWarehouseDetail" class="content-box">
+      <div @click="goToPage('warehouse')" class="content-box">
         <div class="content-box-top"><span class="material-symbols-outlined">warehouse</span></div>
-        <div class="content-box-bottom warehouse"><h3>Warehouse</h3></div>
+        <div class="content-box-bottom warehouse"><h3 class="subtitle">Warehouse</h3></div>
       </div>
-      <div @click="goToProjectDetail" class="content-box">
+      <div @click="goToPage('project')" class="content-box">
         <div class="content-box-top"><span class="material-symbols-outlined">tactic</span></div>
-        <div class="content-box-bottom project"><h3>Project</h3></div>
+        <div class="content-box-bottom project"><h3 class="subtitle">Project</h3></div>
       </div>
-      <div @click="goToTeamDetail" class="content-box">
+      <div @click="goToPage('team')" class="content-box">
         <div class="content-box-top"><span class="material-symbols-outlined">folder_shared</span></div>
-        <div class="content-box-bottom team"><h3>Team</h3></div>
+        <div class="content-box-bottom team"><h3 class="subtitle">Team</h3></div>
       </div>
-      <div @click="goToUserDetail" class="content-box">
+      <div @click="goToPage('user')" class="content-box">
         <div class="content-box-top"><span class="material-symbols-outlined">person</span></div>
-        <div class="content-box-bottom user"><h3>User</h3></div>
+        <div class="content-box-bottom user"><h3 class="subtitle">User</h3></div>
       </div>
     </div>
+    <div class="line rounded mt-3 mb-3"></div>
     <div>
-      <router-view>
-      </router-view>
+      <router-view id="scrollTo" />
     </div>
   </div>
 </template>
 
 <script>
+import NavBarComponent from "@/components/NavBarComponent";
+
 export default {
   name: 'ManageComponent',
+  components: {
+    NavBarComponent,
+  },
   methods: {
-    goToProductDetail(){
-      this.$router.push('/manage/product')
-    },
-    goToProjectDetail(){
-      this.$router.push('/manage/project')
-    },
-    goToWarehouseDetail(){
-      this.$router.push('/manage/warehouse')
-    },
-    goToTeamDetail(){
-      this.$router.push('/manage/team')
-    },
-    goToUserDetail(){
-      this.$router.push('/manage/user')
+    goToPage(page) {
+      let path = '/manage/' + page;
+      if (this.$route.path === path) {
+        this.$router.push('/manage');
+        return;
+      }
+
+      this.$router.push(path);
+      setTimeout(() => {
+        let y = document.getElementById("scrollTo")?.offsetTop;
+        if (y > window.screen.height / 2) {
+          window.scrollTo({top: y, behavior: "smooth"});
+        }
+      }, 1);
     }
   }
 
@@ -103,45 +61,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/*.empty {*/
-/*  width: 0;*/
-/*}*/
+.subtitle {
+  text-align: center;
+  font-size: max(1.5vw, 15px);
+}
 
-/*.container {*/
-/*  display: flex;*/
-/*  flex-direction: column;*/
-/*  height: 100px;*/
-/*  width: 150px;*/
-/*  border: 2px solid black;*/
-/*  margin: 0;*/
-/*  padding: 0;*/
-/*  cursor: pointer;*/
-/*}*/
-
-/*.logoContainer {*/
-/*  display: flex;*/
-/*  align-items: center;*/
-/*  justify-content: center;*/
-/*  height: 70%;*/
-/*}*/
-
-/*.textContainer {*/
-/*  display: flex;*/
-/*  align-items: center;*/
-/*  justify-content: center;*/
-/*  height: 30%;*/
-/*  width: 100%;*/
-/*  border-top: 2px solid black;*/
-/*}*/
-
-/*.material-symbols-outlined {*/
-/*  font-size: 2em;*/
-/*  font-variation-settings:*/
-/*      'FILL' 0,*/
-/*      'wght' 300,*/
-/*      'GRAD' 0,*/
-/*      'opsz' 24*/
-/*}*/
+.line {
+  background-color: lightgray;
+  height: 0.5vh;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
 
 .nav-box{
   display: flex;
@@ -152,12 +83,12 @@ export default {
   align-content: center;
 }
 
-.content-box{
-  height: 150px;
-  width: 200px;
+.content-box {
+  height: max(10vw, 100px);
+  width: max(15vw, 150px);
   border: 3px solid lightgray;
   border-radius: 10px;
-  margin: 15px;
+  margin: 1vw;
   flex-shrink: 0;
 }
 
@@ -177,12 +108,11 @@ export default {
   border-top: 3px solid lightgray;
   padding: 5px;
   height: 30%;
-  text-align: center;
   border-radius: 0 0 7px 7px;
 }
 
 .material-symbols-outlined{
-  font-size: 70px;
+  font-size: max(4vw, 40px);
   color: black;
 }
 
