e8584a10cb4627067781b51790c48dbb0f87db3a
Kevin Bakker <kevin.bakker@hva.nl>
2023-10-08 17:44:59 +0200

    -fix error

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,20 +2,20 @@ import {createRouter, createWebHistory} from 'vue-router';
 
 import LoginComponent from "@/components/LoginComponent.vue";
 import OverviewComponent from "@/components/OverviewComponent.vue";
-import ProductDetailComponent from "@/components/manage/ProductDetailComponent.vue";
-import ProjectDetailComponent from "@/components/manage/ProjectDetailComponent.vue";
 import WarehouseDetailComponent from "@/components/manage/WarehouseDetailComponent.vue";
 import TeamDetailComponent from "@/components/manage/TeamDetailComponent.vue";
 import NotFoundComponent from "@/components/NotFoundComponent.vue";
+import ProjectEditComponent from "@/components/manage/ProjectEditComponent.vue";
+import ProductEditComponent from "@/components/manage/ProductEditComponent.vue";
 
 const routes = [
     {path: '/', redirect: '/login'},
     {path: '/login', component: LoginComponent},
     {path: '/overview', component: OverviewComponent},
     {path: '/warehouses', component: WarehouseDetailComponent},
-    {path: '/projects', component: ProjectDetailComponent},
+    {path: '/projects', component: ProjectEditComponent},
     {path: '/teams', component: TeamDetailComponent},
-    {path: '/products', component: ProductDetailComponent},
+    {path: '/products', component: ProductEditComponent},
     {path: '/:pathMatch(.*)*', component: NotFoundComponent}
 ];
 export const router = createRouter({
