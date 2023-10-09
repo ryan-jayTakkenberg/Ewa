718e522575330e42518d09f75012df8c016511be
Kevin Bakker <kevin.bakker@hva.nl>
2023-10-09 18:15:23 +0200

    -Bootstrap off for now (clashing with tailwind css) -Added extra general components

--- frontend/src/components/UsersComponent.vue
+++ frontend/src/components/UsersComponent.vue
@@ -1,17 +1,19 @@
-
 <template>
   <TitleComponent page-title="Users"></TitleComponent>
+  <SearchBarComponent search-bar-text="Search Users"></SearchBarComponent>
 
 </template>
 
 <script>
 
 import TitleComponent from "@/general/TitleComponent.vue";
+import SearchBarComponent from "@/general/SearchBarComponent.vue";
 
 export default {
   name: "UsersComponent",
   components: {
-    TitleComponent
+    TitleComponent,
+    SearchBarComponent
   },
 }
 </script>
