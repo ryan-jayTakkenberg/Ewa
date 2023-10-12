f486e9d30afad8c652f8272670568e73c88ae92e
Kevin Bakker <kevin.bakker@hva.nl>
2023-10-12 21:15:29 +0200

    - Fixed Routing

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,8 +7,8 @@ import TeamDetailComponent from "@/components/manage/TeamDetailComponent.vue";
 import NotFoundComponent from "@/components/NotFoundComponent.vue";
 import ProductDetailComponent from "@/components/manage/ProductDetailComponent";
 import ProjectDetailComponent from "@/components/manage/ProjectDetailComponent";
-import UsersComponent from "@/components/UsersViewComponent.vue";
 import ReportComponent from "@/components/manage/ReportComponent.vue";
+import UsersViewComponent from "@/components/users/UsersViewComponent.vue";
 
 const routes = [
     {path: '/', redirect: '/overview'},
@@ -18,7 +18,7 @@ const routes = [
     {path: '/projects', component: ProjectDetailComponent},
     {path: '/teams', component: TeamDetailComponent},
     {path: '/products', component: ProductDetailComponent},
-    {path: '/users', component: UsersComponent},
+    {path: '/users', component: UsersViewComponent},
     {path: '/reports', component: ReportComponent},
     {path: '/:pathMatch(.*)*', component: NotFoundComponent}
 ];
