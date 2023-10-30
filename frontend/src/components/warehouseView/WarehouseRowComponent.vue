f8b5dec25938a5895414e8a2d4e921ac9039d9d3
Ryan-Jay Takkenberg <Ryan-jay.takkenb erg@hva.nl>
2023-10-30 17:57:43 +0100

    back-end gewerkt voor teams allen kan niet bij de users door de UUID

--- frontend/src/components/warehouseView/WarehouseRowComponent.vue
+++ frontend/src/components/warehouseView/WarehouseRowComponent.vue
@@ -3,12 +3,7 @@
     <!-- Check box -->
     <td class="w-4 p-4">
       <div class="flex items-center">
-        <input
-            type="checkbox"
-            :checked="checked"
-            @change="toggleCheckbox"
-            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
-        >
+      
       </div>
     </td>
 
