032d23fda0c2db229d7704a8aa34091c407a6e7f
Tobi <tobi.kok@hva.nl>
2023-11-13 21:46:49 +0100

    ViewerOverview V1.0

--- frontend/src/components/overview/ViewerOverviewComponent.vue
+++ frontend/src/components/overview/ViewerOverviewComponent.vue
@@ -8,8 +8,51 @@ export default {
       userName: 'Jason',
       meetingTime: '11:30 - 12:30',
       meetingLocation: 'Warehouse 2',
+      selectedMessages: [],
+
+      projects: [
+        { title: 'Project Green', team: '1' },
+        { title: 'Project Blue', team: '2' },
+        { title: 'Project Red', team: '1' },
+        { title: 'Project Yellow', team: '3' },
+      ],
+      projectDescriptions: [
+        { title: 'Planned from: 17/10/2023 to 25/10/2023' },
+        { title: 'Planned from: 19/10/2023 to 23/10/2023' },
+        { title: 'Planned from: 20/10/2023 to 27/10/2023' },
+        { title: 'Planned from: 25/10/2023 to 31/10/2023' },
+      ],
+      sampleMessages: [
+        {
+          sender: 'Admin',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam distinctio, ducimus, fugiat incidunt natus non porro quasi qui recusandae tempora tempore vero vitae voluptatibus! Dolorem esse nemo soluta voluptatem voluptatum.'
+        },
+        {
+          sender: 'Admin',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam distinctio, ducimus, fugiat incidunt natus non porro quasi qui recusandae tempora tempore vero vitae voluptatibus! Dolorem esse nemo soluta voluptatem voluptatum.'
+        },
+        {
+          sender: 'Warehouse 2',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam distinctio, ducimus, fugiat incidunt natus non porro quasi qui recusandae tempora tempore vero vitae voluptatibus! Dolorem esse nemo soluta voluptatem voluptatum.'
+        },
+      ],
       }
     },
+
+  methods: {
+    getRandomColor() {
+      const colors = ['#00d315', '#ff0000'];
+      const randomIndex = Math.floor(Math.random() * colors.length);
+      return colors[randomIndex];
+    },
+    toggleSelected(index) {
+      if (this.selectedMessages.includes(index)) {
+        this.selectedMessages = this.selectedMessages.filter((item) => item !== index);
+      } else {
+        this.selectedMessages.push(index);
+      }
+    },
+  },
 
   computed: {
     dayOfTheWeek() {
@@ -22,7 +65,7 @@ export default {
     numberOfTheDay() {
       const today = new Date();
       return today.getUTCDate()
-    }
+    },
   },
 
 }
@@ -45,26 +88,79 @@ export default {
   </div>
 
   <!--- Agenda ---------------------------------------------------------------------------------->
-  <div class="agendaContainer">
+  <div class="sectionContainer agenda">
 
     <div class="dateContainer">
-      <div class="date">
-        <p class="dayOfTheWeek">{{ dayOfTheWeek }}</p>
-        <p class="dayOfTheWeekNum">{{ numberOfTheDay }}</p>
-      </div>
+      <p class="dayOfTheWeek">{{ dayOfTheWeek }}</p>
+      <p class="dayOfTheWeekNum">{{ numberOfTheDay }}</p>
+    </div>
+
       <div class="insightContainer">
-        <div class="meetingInsights">
+        <p class="medium"> 1 meeting today:</p>
+        <div class="meetingWrapper">
           <span class="material-symbols-outlined">schedule</span>
-          <p class="meetingText"> {{ meetingTime }} </p>
+          <div> {{ meetingTime }} </div>
         </div>
-        <div class="meetingInsights">
+        <div class="meetingWrapper">
           <span class="material-symbols-outlined">location_on</span>
-          <p class="meetingText"> {{ meetingLocation }} </p>
+          <div> {{ meetingLocation }} </div>
         </div>
+
+      </div>
+
+  </div>
+
+  <!--- projects ---------------------------------------------------------------------------------->
+  <div class="sectionContainer">
+
+    <h1 class="sectionTitle">Ongoing Projects</h1>
+    <div class="projectContainer">
+
+      <div class="projectWrapper" v-for="(project, index) in projects" :key="index">
+        <div class="projectHeader">
+          <div class="projectTitle"> {{ project.title }}</div>
+          <div class="statusWrapper">
+            <div class="projectStatus"> Status: </div>
+            <div :style="{ backgroundColor: getRandomColor() }" class="statusColor"></div>
+          </div>
+        </div>
+        <div class="projectDescription">{{ projectDescriptions[index].title }}<br>
+              Team: {{ project.team }}</div>
+        <p>Click for more details</p>
+      </div>
+
+    </div>
+  </div>
+
+  <!--- messages ---------------------------------------------------------------------------------->
+  <div class="sectionContainer">
+
+    <div class="messageHeader">
+      <h1 class="sectionTitle">Messages</h1>
+      <div class="buttonContainer">
+        <button class="deleteMessage">
+          <span class="material-symbols-outlined button">delete</span>
+        </button>
+        <button class="filterMessage">
+          <span class="material-symbols-outlined button">filter_alt</span>
+        </button>
+      </div>
+    </div>
+
+    <div class="messageContainer">
+      <div
+          class="messageWrapper"
+          v-for="(message, index) in sampleMessages"
+          :key="index"
+          @click="toggleSelected(index)"
+          :class="{ 'selected': selectedMessages.includes(index) }">
+        <div class="messageSender"> {{ message.sender }} </div>
+        <div class="message"> {{ message.content }} </div>
       </div>
     </div>
 
   </div>
+
 
 </template>
 
@@ -75,7 +171,7 @@ export default {
   align-items: center;
   justify-content: space-between;
   width: 100%;
-  padding: 2rem 3rem;
+  padding: 2rem 3rem 0 3rem;
 }
 
 .welcomeContainer {
@@ -93,7 +189,7 @@ h1 {
 .description {
   font-size: 1rem;
   font-weight: 400;
-  color: #ccc;
+  color: #aaa;
 }
 
 .profilePicContainer {
@@ -103,70 +199,191 @@ h1 {
   border-radius: 50%;
 }
 
-.agendaContainer {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+.sectionContainer {
   width: 100%;
   padding: 2rem 3rem;
   border-bottom: 2px solid #e5e5e5;
 }
 
-.dateContainer {
+.agenda {
   display: flex;
-  width: 100%;
-  height: 200px;
-  border: 2px solid #e5e5e5;
-  border-radius: 15px;
 }
 
-.date {
+.dateContainer {
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  height: 100%;
-  width: 15%;
-  border-right: 2px solid #e5e5e5;
+  width: 150px;
+  height: 150px;
+  border: 2px solid #e5e5e5;
+  border-radius: 15px;
 }
 
 .dayOfTheWeek {
-  font-size: 2rem;
+  font-size: 1.5rem;
   font-weight: 700;
-  line-height: 1;
+  line-height: 0.8;
   color: #222;
 }
 
 .dayOfTheWeekNum {
-  font-size: 4rem;
+  font-size: 3rem;
   font-weight: 800;
-  line-height: 1;
+  line-height: 0.8;
   color: #222;
 }
 
 .insightContainer {
   display: flex;
-  align-items: center;
-  justify-content: center;
   flex-direction: column;
-  gap: 1rem;
-  height: 100%;
-  width: 15%;
-  padding: 2rem;
+  padding: 1rem;
 }
 
-.meetingInsights {
+.meetingWrapper {
   display: flex;
   align-items: center;
   justify-content: left;
-  width: 100%;
+  gap: 0.5rem;
+  flex-direction: row;
 }
 
-.meetingText {
-  margin-left: 1rem;
-  font-size: 1rem;
-  font-weight: 500;
+.medium {
+  font-weight: 600;
+  margin-bottom: 0.5rem;
+}
+
+.sectionTitle {
+  text-align: left;
+  font-size: 1.5rem;
+  font-weight: 700;
   color: #222;
+}
+
+.projectContainer {
+  display: flex;
+  gap: 1rem;
+  width: 100%;
+  height: auto;
+  padding: 1rem 0.15rem;
+  overflow-x: scroll;
+  margin-top: 1rem;
+}
+
+.projectWrapper {
+  min-width: 500px;
+  width: auto;
+  flex: 0 0 auto;
+  background: #f5f5f5;
+  border-radius: 5px;
+  padding: 1rem;
+  cursor: pointer;
+}
+
+.projectWrapper:hover {
+  outline: 2px solid #222;
+}
+
+.projectHeader {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 2rem;
+}
+
+.projectTitle {
+  font-size: 1.2rem;
+  font-weight: 700;
+  color: #222;
+}
+
+.statusWrapper {
+  display: flex;
+  align-items: center;
+}
+
+.projectStatus {
+  font-size: 1.2rem;
+  font-weight: 400;
+  color: #222;
+}
+
+.statusColor {
+  height: 20px;
+  width: 20px;
+  border-radius: 50%;
+  margin-left: 0.5rem;
+}
+
+p {
+  margin-top: 1rem;
+  font-weight: 300;
+  color: #222;
+}
+
+.messageContainer {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 2rem;
+  height: 500px;
+  width: 50%;
+  background: #f5f5f5;
+  border-radius: 10px;
+  overflow-y: scroll;
+}
+
+.messageHeader {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 50%;
+  margin-bottom: 1rem;
+}
+
+.messageWrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 100%;
+  min-height: 150px;
+  height: auto;
+  flex: 0 0 auto;
+  background: #fff;
+  padding: 1rem;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.messageSender {
+  width: 100%;
+  font-weight: 600;
+  color: #222;
+  border-bottom: 1px solid #e5e5e5;
+}
+
+.selected {
+  background-color: #e5e5e5;
+}
+
+.buttonContainer {
+  display: flex;
+  gap: 1rem;
+}
+
+button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  width: 50px;
+  background: #f5f5f5;
+  outline: none;
+  cursor: pointer;
+}
+
+button:hover {
+  outline: 2px solid #222;
 }
 
 </style>
