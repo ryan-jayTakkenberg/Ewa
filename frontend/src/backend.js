8a6da7b3bffbce540f3bda7591257adf3acabfcc
Leon Dolmans <leon.dolmans@hva.nl>
2023-11-18 20:37:59 +0100

    added backend.js

---
+++ frontend/src/backend.js
@@ -0,0 +1,34 @@
+import axios from "./axios-config";
+import {getJWT} from "@/data";
+
+export async function getAPI(endpoint) {
+    return await axios.get(endpoint, {
+        headers: {
+            "Authorization": getJWT(),
+        },
+    }).catch(r => r?.response);
+}
+
+export async function postAPI(endpoint, data = {}) {
+    return await axios.post(endpoint, data, {
+        headers: {
+            "Authorization": getJWT(),
+        },
+    }).catch(r => r?.response);
+}
+
+export async function putAPI(endpoint, data = {}) {
+    return await axios.put(endpoint, data, {
+        headers: {
+            "Authorization": getJWT(),
+        },
+    }).catch(r => r?.response);
+}
+
+export async function deleteAPI(endpoint) {
+    return await axios.delete(endpoint, {
+        headers: {
+            "Authorization": getJWT(),
+        },
+    }).catch(r => r?.response);
+}
