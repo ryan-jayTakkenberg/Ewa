8b521742e17561d9302a7842c2e081c574d20e69
Leon Dolmans <leon.dolmans@hva.nl>
2023-11-18 21:12:12 +0100

    comments

--- frontend/src/service/teams-adaptor.js
+++ frontend/src/service/teams-adaptor.js
@@ -4,48 +4,64 @@ import {classToObject} from "@/models/helper";
 
 export class TeamsAdaptor {
 
+    // Fetches all teams from the API
     async asyncFindAll() {
         const response = await getAPI("/api/teams");
+
+        // Check if the response is successful
         if (!responseOk(response)) {
             console.error(response.data);
             return [];
         }
 
+        // Check if the response data is a valid JSON array
         if (!Array.isArray(response.data)) {
             console.error('Geen geldige JSON-array ontvangen: ' + response.data);
             return [];
         }
 
+        // Map the response data to Team objects and return the array
         return response.data.map(data => new Team(data.id, data.name, data.warehouse, data.project));
     }
 
+    // Fetches a team by its ID from the API
     async asyncFindById(id) {
         const response = await getAPI(`/api/teams/${id}`);
+
+        // Check if the response is successful
         if (!responseOk(response)) {
             console.error(response.data);
             return {};
         }
 
+        // Return the response data
         return response.data;
     }
 
+    // Saves a new team to the API
     async asyncSaveTeam(team) {
         const response = await postAPI("/api/teams", classToObject(team));
+
+        // Check if the response is successful
         if (!responseOk(response)) {
             console.error(response.data);
             return {};
         }
 
+        // If the response contains an ID, update the team object with it
         if (response.data.id) {
             team.id = response.data.id;
         }
 
+        // Return the response data
         return response.data;
     }
 
-
+    // Updates an existing team on the API
     async asyncUpdateTeam(team) {
         const response = await putAPI(`/api/teams/${team.id}`, classToObject(team));
+
+        // Check if the response is successful
         if (!responseOk(response)) {
             console.error(response.data);
             return {};
@@ -59,19 +75,21 @@ export class TeamsAdaptor {
             return {};
         }
 
+        // Return a new Team object with the updated information
         return new Team(data.id, data.name, data.warehouse, data.project);
     }
 
-
-
-
+    // Deletes a team by its ID from the API
     async asyncDeleteById(id) {
         const response = await deleteAPI(`/api/teams/${id}`);
+
+        // Check if the response is successful
         if (!responseOk(response)) {
             console.error(response.data);
             return {};
         }
 
+        // Return the response data
         return response.data;
     }
 }
