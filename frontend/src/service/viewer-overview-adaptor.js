3f706049cdc68f95a0ae15acc4af24d1e07e1295
Tobi <tobi.kok@hva.nl>
2023-11-19 18:51:04 +0100

    Added async method that posts orders in viewer overview

--- frontend/src/service/viewer-overview-adaptor.js
+++ frontend/src/service/viewer-overview-adaptor.js
@@ -1,4 +1,6 @@
-/* export class ViewerOverviewAdaptor {
+import {getAPI} from "@/backend";
+
+export class ViewerOverviewAdaptor {
 
     resourcesURL;
 
@@ -9,51 +11,74 @@
     async fetchViewerName(id) {
 
         try {
-            let response = await fetch(this.resourcesUrl`/${id}`);
+            const response = await getAPI(`/api/users/${id}`);
             console.log(response.status);
-            return await response.json();
+
+            if (response.ok) {
+                const data = await response.data();
+                return data.name;
+            } else {
+                console.error(`Failed to fetch user name: ${response.statusText}`);
+            }
         } catch (error) {
-            console.error(error);
+            console.error("Error fetching user name:", error);
         }
     }
+
 
     async fetchViewerTeam(id) {
 
         try {
-            let response = await fetch(this.resourcesUrl`/${id}`);
+            const response = await getAPI(`/api/teams/${id}`);
             console.log(response.status);
-            return await response.json();
+
+            if (response.ok) {
+                const data = await response.data();
+                return data.name;
+            } else {
+                console.error(`Failed to fetch user name: ${response.statusText}`);
+            }
         } catch (error) {
-            console.error(error);
+            console.error("Error fetching user name:", error);
         }
     }
 
     async fetchViewerProjects(id) {
 
         try {
-            let response = await fetch(this.resourcesUrl`/${id}`);
+            const response = await getAPI(`/api/projects/${id}`);
             console.log(response.status);
-            return await response.json();
+
+            if (response.ok) {
+                return await response.data();
+            } else {
+                console.error(`Failed to fetch user name: ${response.statusText}`);
+            }
         } catch (error) {
-            console.error(error);
+            console.error("Error fetching user name:", error);
         }
     }
 
     async fetchViewerReports(id) {
 
         try {
-            let response = await fetch(this.resourcesUrl`/${id}/reports`);
+            const response = await getAPI(`/api/projects/${id}`);
             console.log(response.status);
-            return await response.json();
+
+            if (response.ok) {
+                return await response.data();
+            } else {
+                console.error(`Failed to fetch user name: ${response.statusText}`);
+            }
         } catch (error) {
-            console.error(error);
+            console.error("Error fetching user name:", error);
         }
     }
 
     async postViewerReports(id, reportData) {
 
         try {
-            let response = await fetch(`${this.resourcesUrl}/${id}/reports`, {
+            let response = await fetch(`${this.resourcesUrl}/reports/${id}`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -73,5 +98,29 @@
             console.error(error);
         }
     }
+
+    async postOrders(id, orderData) {
+
+        try {
+            let response = await fetch(`${this.resourcesUrl}/orders/${id}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    // Include additional headers, such as authentication headers
+                },
+                body: JSON.stringify(orderData),
+            });
+
+            console.log(response.status);
+
+            if (response.ok) {
+                return await response.json();
+            } else {
+                console.error('Failed to post viewer report:', response.statusText);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
 }
-*/
