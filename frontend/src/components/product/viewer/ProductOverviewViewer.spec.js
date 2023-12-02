94fbcde9bc6b56deec809eb95991edf14a2db76b
Leon Dolmans <leon.dolmans@hva.nl>
2023-12-02 16:22:52 +0100

    tests

--- frontend/src/components/product/viewer/ProductOverviewViewer.spec.js
+++ frontend/src/components/product/viewer/ProductOverviewViewer.spec.js
@@ -4,12 +4,13 @@ import ProductOverviewViewer from './ProductOverviewViewer';
 let wrapper;
 
 const SEARCHBAR_PLACEHOLDER_CONTAINS = 'search';
+const TABLE_HEADER_CONTAINS = 'product';
 
 beforeEach(function() {
     wrapper = mount(ProductOverviewViewer);
 });
 
-it('creates a proper main page structure', function() {
+it('creates a proper page structure', function() {
     expect(wrapper.element.children.length,
         `main page starting with ${wrapper.element.tagName} has no child elements`)
         .toBeGreaterThan(0);
@@ -27,4 +28,14 @@ it('has searchbar', async function() {
     expect(searchbar.element.disabled,
         `Searchbar element is disabled`)
         .toBeFalsy();
+});
+
+it('has table', async function() {
+    // find the button to click
+    const productTableHeader = wrapper.findAll('th')
+        .find(th => th.element.innerHTML?.toLowerCase().includes(TABLE_HEADER_CONTAINS.toLowerCase()));
+
+    expect(productTableHeader?.exists(),
+        `Viewer is missing a product table`)
+        .toBe(true);
 });
