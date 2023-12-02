e30fabdd5d9c4d3aadacba1a43fc12d8b2e0c377
Leon Dolmans <leon.dolmans@hva.nl>
2023-12-02 01:07:31 +0100

    comments

--- backend/esserver/src/main/java/app/controllers/ProductController.java
+++ backend/esserver/src/main/java/app/controllers/ProductController.java
@@ -5,61 +5,66 @@ import app.exceptions.ForbiddenException;
 import app.jwt.JWToken;
 import app.models.Product;
 import app.repositories.ProductJPARepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * PRODUCT CONTROLLER
+ * <br />This controller class manages the CRUD (Create, Read, Update, Delete) operations for the 'Product' entity.
+ * @apiNote Endpoints:
+ * <br />- GET /product: Retrieves a list of all products. Requires a valid JWT token for authentication.
+ * <br />- POST /product: Creates a new product. Requires an admin-level JWT token for authorization.
+ * <br />- DELETE /product/{id}: Removes a product by ID. Requires an admin-level JWT token for authorization.
+ * <br />
+ * <br />Authorization:
+ * <br />- All endpoints require a valid JWT token, and a ForbiddenException is thrown if no token is provided.
+ * <br />- Admin-level token is required for creating and deleting products; otherwise, a ForbiddenException is thrown.
+ * <br />
+ * <br />Error Handling:
+ * <br />- Throws ForbiddenException for authentication and authorization issues.
+ * <br />- Throws BadRequestException for invalid or missing parameters.
+ * <br />
+ * <br />Dependencies:
+ * <br />- Autowired 'ProductJPARepository' for database interaction.
+ * <br />- Utilizes 'JWToken' for extracting JWT information from the request attributes.
+ * <br />
+ * <br />Note:
+ * <br />- The controller assumes a REST structure and adheres to HTTP status codes.
+ * <br />- Ensure that the 'ProductJPARepository' is properly configured for database operations.
+ * <br />- The POST /product can also be used to edit (usually PUT) the product
+ * @author Leon
+ */
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
-    /*
-     * PRODUCT CONTROLLER
-     * This controller class manages the CRUD (Create, Read, Update, Delete) operations for the 'Product' entity.
-     *
-     * Endpoints:
-     * - GET /product: Retrieves a list of all products. Requires a valid JWT token for authentication.
-     * - POST /product: Creates a new product. Requires an admin-level JWT token for authorization.
-     * - DELETE /product/{id}: Removes a product by ID. Requires an admin-level JWT token for authorization.
-     *
-     * Authorization:
-     * - All endpoints require a valid JWT token, and a ForbiddenException is thrown if no token is provided.
-     * - Admin-level token is required for creating and deleting products; otherwise, a ForbiddenException is thrown.
-     *
-     * Error Handling:
-     * - Throws ForbiddenException for authentication and authorization issues.
-     * - Throws BadRequestException for invalid or missing parameters.
-     *
-     * Dependencies:
-     * - Autowired 'ProductJPARepository' for database interaction.
-     * - Utilizes 'JWToken' for extracting JWT information from the request attributes.
-     *
-     * Note:
-     * - The controller assumes a REST structure and adheres to HTTP status codes.
-     * - Ensure that the 'ProductJPARepository' is properly configured for database operations.
-     * - The POST /product can also be used to UPDATE (edit) the product
+    private final ProductJPARepository productRepo;
+
+    public ProductController(ProductJPARepository productRepo) {
+        this.productRepo = productRepo;
+    }
+
+    /**
+     * Getting all the products
+     * @return list of products
      */
-
-    @Autowired
-    private ProductJPARepository productRepo;
-
     @GetMapping
-    private List<Product> getProducts(@RequestAttribute(name = JWToken.JWT_ATTRIBUTE_NAME) JWToken jwtInfo) {
-        if (jwtInfo == null) {
-            throw new ForbiddenException("No token provided");
-        }
-
+    private List<Product> getProducts() {
         return productRepo.findAll();
     }
 
+    /**
+     * Add (or edit) a product to the database
+     * @param jwtInfo the json web token
+     * @param product the product to add or edit
+     * @return the product if it was added or edited successfully
+     * @apiNote requires admin permission
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     private Product postProduct(@RequestAttribute(name = JWToken.JWT_ATTRIBUTE_NAME) JWToken jwtInfo, @RequestBody Product product) {
-        if (jwtInfo == null) {
-            throw new ForbiddenException("No token provided");
-        }
         if (!jwtInfo.isAdmin()) {
             throw new ForbiddenException("Admin role is required to create a product");
         }
@@ -67,11 +72,15 @@ public class ProductController {
         return productRepo.save(product);
     }
 
+    /**
+     * Deletes a product from the database
+     * @param jwtInfo the json web token
+     * @param id the product id
+     * @return the deleted product
+     * @apiNote requires admin permission
+     */
     @DeleteMapping("/{id}")
     private Product deleteProduct(@RequestAttribute(name = JWToken.JWT_ATTRIBUTE_NAME) JWToken jwtInfo, @PathVariable Long id) {
-        if (jwtInfo == null) {
-            throw new ForbiddenException("No token provided");
-        }
         if (!jwtInfo.isAdmin()) {
             throw new ForbiddenException("Admin role is required to remove a product");
         }
@@ -82,7 +91,7 @@ public class ProductController {
 
         Product product = productRepo.findById(id);
         if (product == null) {
-            throw new BadRequestException("No product found for such id");
+            throw new BadRequestException("No product found for such ID");
         }
 
         return productRepo.delete(product);
