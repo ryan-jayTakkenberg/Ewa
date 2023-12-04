83c2229981e678b37139fe114c6cddaab0eabdb9
Tobi Kok <tobi.kok@hva.nl>
2023-12-04 22:52:01 +0100

    +1

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
+// Added for deployment, I think it does nothing but didn't delete it yet. Old config is commented out below.
+
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  publicPath: process.env.NODE_ENV === 'production' ? '/your-production-path/' : '/',
+  transpileDependencies: ['vuetify'], // Add any other dependencies you want to transpile
+
+});
+
+
+
+
+
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
