7a1592a9a72f653643a9ddd139821ca7f095df62
Tobi <tobi.kok@hva.nl>
2023-12-05 20:53:35 +0100

    changes for deployment

--- frontend/src/axios-config.js
+++ frontend/src/axios-config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8085",
+    baseURL: process.env.VUE_APP_API_URL + '/api'
 });
 
 export default instance;
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,18 +1,6 @@
-// Added for deployment, I think it does nothing but didn't delete it yet. Old config is commented out below.
-
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/your-production-path/' : '/',
-  transpileDependencies: ['vuetify'], // Add any other dependencies you want to transpile
-
+  publicPath: '/',
+  transpileDependencies: true
 });
-
-
-
-
-
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
