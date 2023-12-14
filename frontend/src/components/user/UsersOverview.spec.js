860086e379d9c1a2faf3fc1604acf9d66a5180d9
Kevin Bakker <kevin.bakker@hva.nl>
2023-12-14 14:58:55 +0100

    -user test

--- frontend/src/components/user/UsersOverview.spec.js
+++ frontend/src/components/user/UsersOverview.spec.js
@@ -21,48 +21,29 @@ it('has searchbar', () => {
     const searchbar = wrapper.findAll('input')
         .find(searchbar => searchbar.element.placeholder?.toLowerCase().includes(SEARCHBAR_PLACEHOLDER_CONTAINS.toLowerCase()));
 
-    expect(searchbar?.exists(),
-        `Viewer is missing a searchbar`)
-        .toBe(true);
-
-    expect(searchbar.element.disabled,
-        `Searchbar element is disabled`)
-        .toBeFalsy();
+    expect(searchbar?.exists(), `Viewer is missing a searchbar`).toBe(true);
+    expect(searchbar.element.disabled, `Searchbar element is disabled`).toBeFalsy();
 });
 
 it('has create button', () => {
     // find the button to click
     const button = wrapper.findAll('button')
         .find(button => button.element.innerHTML?.toLowerCase().includes(CREATE_BTN.toLowerCase()));
-
-    expect(button?.exists(),
-        `missing create user button`)
-        .toBe(true);
-
-    expect(button.element.disabled,
-        `Create user button is disabled`)
-        .toBeFalsy();
+    expect(button?.exists(), `missing create user button`).toBe(true);
+    expect(button.element.disabled, `Create user button is disabled`).toBeFalsy();
 });
 
 it('has action button', () => {
     // find the button to click
     const button = wrapper.findAll('button')
         .find(button => button.element.innerHTML?.toLowerCase().includes(ACTION_BUTTON.toLowerCase()));
-
-    expect(button?.exists(),
-        `missing action button`)
-        .toBe(true);
-
-    expect(button.element.disabled,
-        `Action button should be disabled`)
-        .toBeTruthy();
+    expect(button?.exists(), `missing action button`).toBe(true);
+    expect(button.element.disabled, `Action button should be disabled`).toBeTruthy();
 });
 
 it('has table', () => {
     // find the button to click
     const userTableHeader = wrapper.findAll('th')
         .find(th => th.element.innerHTML?.toLowerCase().includes(TABLE_HEADER_CONTAINS.toLowerCase()));
-
-    expect(userTableHeader?.exists(),
-        `missing a user table`).toBe(true);
+    expect(userTableHeader?.exists(), `missing a user table`).toBe(true);
 });
