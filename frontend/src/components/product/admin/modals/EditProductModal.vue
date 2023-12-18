a6125013fe57d3ef38f93c667c7a342be2bf00a4
Leon Dolmans <leon.dolmans@hva.nl>
2023-12-18 19:04:21 +0100

    Changed to use SolarModal

--- frontend/src/components/product/admin/modals/EditProductModal.vue
+++ frontend/src/components/product/admin/modals/EditProductModal.vue
@@ -1,67 +1,52 @@
 <template>
-  <!-- Edit modal -->
-  <div class="fixed inset-0 flex justify-center overflow-hidden items-center bg-black bg-opacity-10 ml-16 sm:ml-0" tabindex="0">
-    <div class="relative">
-      <!-- Modal content -->
-      <div class="bg-white rounded-2xl shadow-xl">
-        <div class="border-gray-200 border-b p-6">
-          <!-- Modal header -->
-          <div class="flex justify-between">
-            <h3 class="text-xl font-bold text-gray-900 ">{{ title }}</h3>
-            <button type="button" @click="close" class="text-gray-500 hover:bg-gray-100 hover:text-black">
-              <svg class="w-7 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-              </svg>
-            </button>
-          </div>
-        </div>
-        <div class="p-6">
-          <!-- Modal body -->
-          <div class="grid lg:grid-cols-2 gap-6">
-            <div class="grid-col">
-              <label for="name">Name</label>
-              <input
-                  type="text"
-                  v-model="this.cloned.name"
-                  class="p-2 border-gray-300 text-sm text-gray-800 bg-gray-50 shadow focus:border-blue-600"
-                  placeholder="Name">
-            </div>
-            <div class="grid-col">
-              <label for="email">Price (€)</label>
-              <input
-                  type="number"
-                  v-model="this.cloned.price"
-                  class="p-2 border-gray-300 text-sm text-gray-800 bg-gray-50 shadow focus:border-blue-600"
-                  placeholder="Price">
-            </div>
-          </div>
-          <div class="mt-6">
-            <label for="email">Description</label>
-            <textarea
-                type="text"
-                v-model="this.cloned.description"
-                class="resize-none overflow-y-scroll p-2 border-gray-300 text-sm text-gray-800 bg-gray-50 shadow focus:border-blue-600"
-                placeholder="Description">
-            </textarea>
-          </div>
-          <!-- Modal footer -->
-        </div>
-        <div class="border-gray-200 border-t p-6">
-          <button class="text-white bg-blue-700 py-2 px-4 font-medium hover:bg-blue-800" @click="save">Save Changes</button>
-        </div>
+  <SolarModal title="Edit Product" @close-modal="close">
+    <!-- Modal body -->
+    <div class="grid lg:grid-cols-2 gap-6">
+      <div class="grid-col">
+        <label for="name">Name</label>
+        <input
+            type="text"
+            v-model="this.cloned.name"
+            class="p-2 border-gray-300 text-sm text-gray-800 bg-gray-50 shadow focus:border-blue-600"
+            placeholder="Name">
+      </div>
+      <div class="grid-col">
+        <label for="email">Price (€)</label>
+        <input
+            type="number"
+            v-model="this.cloned.price"
+            class="p-2 border-gray-300 text-sm text-gray-800 bg-gray-50 shadow focus:border-blue-600"
+            placeholder="Price">
       </div>
     </div>
-  </div>
+    <div class="mt-6">
+      <label for="email">Description</label>
+      <textarea
+          type="text"
+          v-model="this.cloned.description"
+          class="resize-none overflow-y-scroll p-2 border-gray-300 text-sm text-gray-800 bg-gray-50 shadow focus:border-blue-600"
+          placeholder="Description">
+            </textarea>
+    </div>
+    <!-- Modal footer -->
+    <template v-slot:footer>
+      <button @click="close" class="text-gray-900 bg-gray-200 font-medium text-sm px-5 py-3 hover:bg-gray-300">Cancel</button>
+      <button @click="save" class="ml-auto text-white bg-blue-700 font-medium text-sm px-5 py-3 hover:bg-blue-800">Save Changes</button>
+    </template>
+  </SolarModal>
 </template>
 
 <script>
+import SolarModal from "@/components/general/SolarModal";
+
 export default {
   name: "EditProductModal",
+  components: {
+    SolarModal,
+  },
   data() {
     return {
       cloned: {},
-      title: "Edit Product"
     }
   },
   props: {
