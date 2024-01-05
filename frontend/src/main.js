b74b2059da857805eae71741944a696a11eb57f6
Tobi Kok <tobi.kok@hva.nl>
2024-01-05 21:56:22 +0100

    removed unused palette

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { router } from './router';
-import './theme/style.css'
+import '@/styles/style.css'
 import './tailwind.css';
 
 createApp(App).use(router).mount('#app');
--- frontend/src/theme/Palette.js
+++
@@ -1,9 +0,0 @@
-//TODO
-export default {
-    mainColor: '#c7d02c',
-    titleColor: '#572700',
-    textColor: '#333333',
-    subtitleColor: '#bfbfbf',
-    textBackground: '#f8f8f8',
-    backgroundColor: '#ffffff',
-};
