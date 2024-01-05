5950e63aabbc299f393c07d21da157076eb93d23
Tobi Kok <tobi.kok@hva.nl>
2024-01-05 18:13:51 +0100

    router clean up

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -12,25 +12,24 @@ import AdminOverviewComponent from "@/components/overview/AdminOverviewComponent
 import OrdersOverview from "@/components/order/OrdersOverview.vue";
 import ProjectOverview from "@/components/project/ProjectOverview.vue";
 import WarehouseOverview from "@/components/warehouse/WarehouseOverview.vue";
-import warehouseOverviewComponent from "@/components/warehouseView/warehouseOverviewComponent.vue";
+import WarehouseOverviewComponent from "@/components/warehouseView/WarehouseOverviewComponent.vue";
 
 const routes = [
     {path: '/', redirect: '/login'},
     {path: '/login', name: 'LOGIN', component: LoginComponent},
-    {path: '/overview', name: 'OVERVIEW'},
     {path: '/viewer-overview', name: 'VIEWER-OVERVIEW', component: ViewerOverviewComponent},
     {path: '/admin-overview', name: 'ADMIN-OVERVIEW', component: AdminOverviewComponent},
-    {path: '/orders', component: OrdersOverview},
-    {path: '/warehouses', component: WarehouseOverview},
-    {path: '/projects', component: ProjectOverview},
-    {path: '/teams', component: TeamsOverview},
-    {path: '/products', component: ProductOverview},
-    {path: '/productViewer', component: ProductOverviewViewer},
-    {path: '/users', component: UsersOverview,},
-    {path: '/reports', component: ReportComponent},
-    {path: '/reset-password', component: PasswordResetComponent },
-    {path: '/warehouseViewer', component: warehouseOverviewComponent},
-    {path: '/:pathMatch(.*)*', component: NotFoundComponent}
+    {path: '/orders', name: 'ORDERS', component: OrdersOverview},
+    {path: '/warehouses', name: 'WAREHOUSES-ADMIN', component: WarehouseOverview},
+    {path: '/warehouseViewer', name: 'WAREHOUSES-VIEWER', component: WarehouseOverviewComponent},
+    {path: '/projects', name: 'PROJECTS', component: ProjectOverview},
+    {path: '/teams', name: 'TEAMS', component: TeamsOverview},
+    {path: '/products', name: 'PRODUCTS-ADMIN', component: ProductOverview},
+    {path: '/productViewer', name: 'PRODUCTS-VIEWER', component: ProductOverviewViewer},
+    {path: '/users', name: 'USERS', component: UsersOverview,},
+    {path: '/reports', name: 'REPORTS', component: ReportComponent},
+    {path: '/reset-password', name: 'RESET-PASSWORD', component: PasswordResetComponent },
+    {path: '/:pathMatch(.*)*', name: 'NOT-FOUND', component: NotFoundComponent}
 ];
 
 export const router = createRouter({
