502c82765d178e84ecf477783e91f296d6803461
Tobi Kok <tobi.kok@hva.nl>
2024-01-08 21:09:39 +0100

    added one jest

--- frontend/src/components/overview/tests/ViewerOverviewComponent.spec.js
+++ frontend/src/components/overview/tests/ViewerOverviewComponent.spec.js
@@ -4,22 +4,14 @@ import ViewerOverviewComponent from '../ViewerOverviewComponent.vue';
 let wrapper;
 
 // Define constants for element selectors
-const REPORTS_CONTAINER = '.reportsContainer';
+const REPORTS_CONTAINER = '.reportsContainerWrapper';
 const SEND_REPORT_BUTTON = '.sendReportButton'
-const DELETE_REPORT_BUTTON = '.deleteMessage';
+const DELETE_REPORT_BUTTON = '.deleteReport';
+const SELECT_RECEIVER = '.reportReceiverSelect';
+const REPORT_TEXT_FIELD = '.reportInput';
 
 beforeEach(() => {
-    wrapper = mount(ViewerOverviewComponent, {
-        global: {
-            provide: {
-                reportService: {
-                    fetchReports: jest.fn(() => Promise.resolve([])),
-                    postReport: jest.fn(() => Promise.resolve()),
-                    deleteReport: jest.fn(() => Promise.resolve()),
-                },
-            }
-        }
-    });
+    wrapper = mount(ViewerOverviewComponent);
 });
 
 afterEach(() => {
@@ -47,6 +39,17 @@ describe('ViewerOverviewComponent', () => {
         const showModalSpy = jest.spyOn(wrapper.vm, 'showModal');
         await wrapper.vm.showModal();
         expect(showModalSpy).toHaveBeenCalled();
+    });
+
+    it('has a send report container', () => {
+        const selectUserInput = wrapper.find(SELECT_RECEIVER);
+        expect(selectUserInput.exists()).toBe(true);
+
+        const reportInput = wrapper.find(REPORT_TEXT_FIELD);
+        expect(reportInput.exists()).toBe(true);
+
+        const sendReportButton = wrapper.find(SEND_REPORT_BUTTON);
+        expect(sendReportButton.exists()).toBe(true);
     });
 
 });
