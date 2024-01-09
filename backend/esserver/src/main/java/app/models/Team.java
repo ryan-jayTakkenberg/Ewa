48fc6e419d2a8113bf0453264fca5dccef1ae781
hamza <hamza.zaraoui@hva.nl>
2024-01-09 01:27:15 +0100

    fixed creation of warehouse

--- backend/esserver/src/main/java/app/models/Team.java
+++ backend/esserver/src/main/java/app/models/Team.java
@@ -21,6 +21,7 @@ public class Team {
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "warehouse_id")
     @JsonIncludeProperties({"id", "name"})
+    @JsonBackReference
     private Warehouse warehouse;
 
     @OneToOne(mappedBy = "team", cascade = {CascadeType.MERGE, CascadeType.REMOVE})
