3ec51e6918aed7af23b4c2851494a3a97b6905b0
Leon Dolmans <leon.dolmans@hva.nl>
2024-01-23 12:16:51 +0100

    .

--- backend/esserver/src/test/java/app/repositories/ProductJPARepositoryTests.java
+++ backend/esserver/src/test/java/app/repositories/ProductJPARepositoryTests.java
@@ -32,6 +32,36 @@ class ProductJPARepositoryTests {
         assertNotNull(productRepo);
     }
 
+//    @Test
+//    void testRepoCRUD() {
+//        // create
+//        final String PRODUCT_NAME = "testName";
+//        Product product = new Product(PRODUCT_NAME, 0, "description");
+//        assertTrue(product.getId() <= 0);
+//        Product savedProduct = productRepo.save(product);
+//        assertTrue(savedProduct.getId() > 0);
+//        assertEquals(savedProduct.getName(), PRODUCT_NAME);
+//
+//        // read
+//        assertEquals(productRepo.findAll(), List.of(savedProduct));
+//        assertEquals(productRepo.findById(savedProduct.getId()), savedProduct);
+//
+//        // update
+//        final String UPDATED_PRODUCT_NAME = "updatedName";
+//        savedProduct.setName(UPDATED_PRODUCT_NAME);
+//        Product updatedProduct = productRepo.save(savedProduct);
+//        assertEquals(productRepo.findAll(), List.of(updatedProduct));
+//        assertEquals(productRepo.findById(updatedProduct.getId()), updatedProduct);
+//        assertEquals(updatedProduct.getName(), UPDATED_PRODUCT_NAME);
+//        assertNotEquals(updatedProduct.getName(), PRODUCT_NAME);
+//
+//        // delete
+//        assertFalse(productRepo.findAll().isEmpty());
+//        Product deletedProduct = productRepo.delete(updatedProduct);
+//        assertNull(productRepo.findById(deletedProduct.getId()));
+//        assertTrue(productRepo.findAll().isEmpty());
+//    }
+
 
 
 }
